3f6a50e65b8c867a7e19f25d1051aa4fb46fefc1
R_Akbar0v <ravshanbekakbarov606@gmail.com>
2024-06-04 16:04:12 +0500

    tugadi

---
+++ js/index.js
@@ -0,0 +1,324 @@
+// 1-masala
+let a = +prompt("a sonni kiriting");
+let result = a > 0;
+console.log(result);
+// 2-masala
+// let a = +prompt("a sonni kiriting");
+// let result = a % 2 == 1;
+// console.log(result);
+// 3-masala
+// let a = +prompt("a sonni kiriting");
+// let result = a % 2 == 0;
+// console.log(result);
+// 4-masala
+// let a = +prompt("a sonni kiriting");
+// let b = +prompt("b sonni kiriting");
+// let result = a > 2 && b <= 3;
+// console.log(result);
+// 5-masala
+// let a = +prompt("a sonni kiriting");
+// let b = +prompt("b sonni kiriting");
+// let result = a >= 2 && b < -2;
+// console.log(result);
+// 6-masala
+// let a = +prompt("a sonni kiriting");
+// let b = +prompt("b sonni kiriting");
+// let c = +prompt("c sonni kiriting");
+// let result = a <= b && b <= c;
+// console.log(result);
+// 7-masala
+// let a = +prompt("a sonni kiriting");
+// let b = +prompt("b sonni kiriting");
+// let c = +prompt("c sonni kiriting");
+// let result = a < b && b < c;
+// console.log(result);
+// 8-masala
+// let a = +prompt("a sonni kiriting");
+// let b = +prompt("b sonni kiriting");
+// let result = a % 2 == 1 && b % 2 == 1;
+// console.log(result);
+// 9-masala
+// let a = +prompt("a sonni kiriting");
+// let b = +prompt("b sonni kiriting");
+// let result = a % 2 == 1 || b % 2 == 1;
+// console.log(result);
+// 10-masala
+// let a = +prompt("a sonni kiriting");
+// let b = +prompt("b sonni kiriting");
+// let result = a % 2 == 1 && b % 2 == 0;
+// console.log(result);
+// 11-masala
+// let a = +prompt("a sonni kiriting");
+// let b = +prompt("b sonni kiriting");
+// let result = (a % 2 == 1 && b % 2 == 1) || (a % 2 == 0 && b % 2 == 0);
+// console.log(result);
+// 12-masala
+// let a = +prompt("a sonni kiriting");
+// let b = +prompt("b sonni kiriting");
+// let c = +prompt("c sonni kiriting");
+// let result = a > 0 && b > 0 && c > 0;
+// console.log(result);
+// 13-masala
+// let a = +prompt("a sonni kiriting");
+// let b = +prompt("b sonni kiriting");
+// let c = +prompt("c sonni kiriting");
+// let result = a > 0 || b > 0 || c > 0;
+// console.log(result);
+// 14-masala
+// let a = +prompt("a sonni kiriting");
+// let b = +prompt("b sonni kiriting");
+// let c = +prompt("c sonni kiriting");
+// let result =
+//   a > 0 ||
+//   b < 0 ||
+//   (c < 0 && a < 0) ||
+//   b < 0 ||
+//   (c > 0 && a < 0 && b < 0) ||
+//   c < 0;
+// console.log(result);
+// 15-masala
+// let a = +prompt("a sonni kiriting");
+// let b = +prompt("b sonni kiriting");
+// let c = +prompt("c sonni kiriting");
+// let result =
+//   (a > 0 && b > 0 && c < 0) ||
+//   (a > 0 && b < 0 && c > 0) ||
+//   (a < 0 && b > 0 && c > 0);
+// console.log(result);
+// 16-masala
+// let a = +prompt("2 xonali sonni kiriting");
+// let result = a > 9 && a < 100 && a % 2 == 0;
+// console.log(result);
+// if ga
+// 1 masala
+// let a = +prompt("a sonni kiriting");
+// if (a > 0) {
+//   let result = a + 1;
+// } else {
+//   let result = a;
+// }
+// console.log(result);
+// 2 masala
+// let a = +prompt("a sonni kiriting");
+// if (a > 0) {
+//   let result = a + 1;
+//   console.log(result);
+// } else {
+//   let result = a - 2;
+//   console.log(result);
+// }
+// 3 masala
+// let a = +prompt("a sonni kiriting");
+// let result = "";
+// if (a > 0) {
+//   result = a + 1;
+// } else if (a < 0) {
+//   result = a - 2;
+// } else {
+//   result = a + 10;
+// }
+// console.log(result);
+// 4-masala
+// let a = +prompt("a sonni kiriting");
+// let b = +prompt("b sonni kiriting");
+// let c = +prompt("c sonni kiriting");
+// let result = 0;
+// if (a > 0) {
+//   result = result + 1;
+// }
+// if (b > 0) {
+//   result = result + 1;
+// }
+// if (c > 0) {
+//   result = result + 1;
+// }
+// console.log(result);
+// 5-masala
+// let a = +prompt("a sonni kiriting");
+// let b = +prompt("b sonni kiriting");
+// let c = +prompt("c sonni kiriting");
+// let resultmusbat = 0;
+// let resultmanfiy = 0;
+
+// if (a > 0) {
+//   resultmusbat = resultmusbat + 1;
+// } else {
+//   resultmanfiy = resultmanfiy + 1;
+// }
+// if (b > 0) {
+//   resultmusbat = resultmusbat + 1;
+// } else {
+//   resultmanfiy = resultmanfiy + 1;
+// }
+// if (c > 0) {
+//   resultmusbat = resultmusbat + 1;
+// } else {
+//   resultmanfiy = resultmanfiy + 1;
+// }
+// console.log("musbat sonlar" + resultmusbat);
+// console.log("manfiy sonlar" + resultmanfiy);
+// 6-masala
+// let a = +prompt("a sonni kiriting");
+// let b = +prompt("b sonni kiriting");
+// let result = "";
+// if (a > b) {
+//   result = a;
+// } else {
+//   result = b;
+// }
+// console.log(result);
+// 7-masala
+// let a = +prompt("a sonni kiriting");
+// let b = +prompt("b sonni kiriting");
+// let result = "";
+// if (a > b) {
+//   result = 2;
+// } else {
+//   result = 1;
+// }
+// console.log(result);
+// 8-masala
+// let a = +prompt("a sonni kiriting");
+// let b = +prompt("b sonni kiriting");
+// let resultkatta = "";
+// let resultkichik = "";
+// if (a > b) {
+//   resultkatta = a;
+// }
+// if (b > a) {
+//   resultkatta = b;
+// }
+// resultkichik = b;
+
+// console.log(resultkatta);
+// console.log(resultkichik);
+// 9-masala
+// let a = +prompt("a sonni kiriting");
+// let b = +prompt("b sonni kiriting");
+// let resulta = "";
+// let resultb = "";
+// if (a > b) {
+//   resultb = b;
+// }
+// if (b > a) {
+//   resulta = a;
+// }
+// resultb = b;
+// resulta = a;
+
+// console.log(resultb);
+// console.log(resulta);
+// 10-masala
+// let a = +prompt("a sonni kiriting");
+// let b = +prompt("b sonni kiriting");
+// let result = "";
+// if (a > b || b > a) {
+//   result = a + b;
+// } else if (a == b) {
+//   result = 0;
+// }
+// console.log(result);
+// 11-masala
+// let a = +prompt("a sonni kiriting");
+// let b = +prompt("b sonni kiriting");
+// let result = "";
+// if (a > b) {
+//   result = a;
+// } else if (a < b) {
+//   result = b;
+// } else if (a == b) {
+//   result = 0;
+// }
+// console.log(result);
+// 12-masala
+// let a = +prompt("a sonni kiriting");
+// let b = +prompt("b sonni kiriting");
+// let c = +prompt("c sonni kiriting");
+// let result = "";
+// if (a > b && b > c) {
+//   result = c;
+// } else if (a > b && c > b) {
+//   result = b;
+// } else if (b > a && a < c) {
+//   result = a;
+// }
+// console.log(result);
+// 13-masala
+// let a = +prompt("a sonni kiriting");
+// let b = +prompt("b sonni kiriting");
+// let c = +prompt("c sonni kiriting");
+// let result = "";
+// if (a > b && b > c) {
+//   result = b;
+// }
+// if (a > b && c > b && a > c) {
+//   result = c;
+// } else {
+//   result = a;
+// }
+// if (b > a && a < c && b > c) {
+//   result = c;
+// } else {
+//   result = b;
+// }
+// console.log(result);
+// 14-masala
+// let a = +prompt("a sonni kiriting");
+// let b = +prompt("b sonni kiriting");
+// let c = +prompt("c sonni kiriting");
+// let resultkichik = "";
+// let resultkatta = "";
+// if (a > b && b > c) {
+//   resultkichik = c;
+// } else {
+//   resultkatta = a;
+// }
+// if (a > b && c > b && a > c) {
+//   resultkichik = b;
+//   resultkatta = a;
+// } else {
+//   resultkatta = c;
+// }
+// if (b > a && a < c && b > c) {
+//   resultkichik = a;
+//   resultkatta = b;
+// } else {
+//   resultkatta = c;
+// }
+// resultkichik = c;
+// resultkichik = b;
+// resultkichik = a;
+
+// console.log(resultkichik);
+// console.log(resultkatta);
+// 15-masala
+// let a = +prompt("a sonni kiriting");
+// let b = +prompt("b sonni kiriting");
+// let c = +prompt("c sonni kiriting");
+// // 15-masala
+// let a = +prompt("a sonni kiriting");
+// let b = +prompt("b sonni kiriting");
+// let c = +prompt("c sonni kiriting");
+// resultorta = "";
+// resultkatta = "";
+// if (a > b && b > c) {
+//   resultorta = b;
+//   resultkatta = a;
+// }
+
+// if (a > b && c > b && a > c) {
+//   resultorta = c;
+//   resultkatta = a;
+// } else {
+//   resultorta = a;
+//   resultkatta = c;
+// }
+// if (b > a && a < c && b > c) {
+//   resultorta = c;
+//   resultkatta = b;
+// } else {
+//   resultorta = b;
+//   resultkatta = c;
+// }
+// let yigindi = resultorta + resultkatta;
+// console.log(yigindi);
